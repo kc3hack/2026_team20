8e68381456b41c892d0f5334c002732cfd309139
mutyuki <belandinging01@gmail.com>
2026-02-18 05:12:38 +0900

    fix: shadcnの見た目が適用されていなかった問題を修正

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import Providers from "@/providers/Providers";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Plot Platform",
