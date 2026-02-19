90cdeb8b9addb9b5e4a73c419d7c916f6b94e964
mutyuki <belandinging01@gmail.com>
2026-02-20 04:49:21 +0900

    feat(api): add adminRepository with tests and barrel exports

---
+++ frontend/src/lib/api/repositories/index.ts
@@ -0,0 +1,9 @@
+export * as adminRepository from "./adminRepository";
+export * as authRepository from "./authRepository";
+export * as historyRepository from "./historyRepository";
+export * as imageRepository from "./imageRepository";
+export * as plotRepository from "./plotRepository";
+export * as searchRepository from "./searchRepository";
+export * as sectionRepository from "./sectionRepository";
+export * as snapshotRepository from "./snapshotRepository";
+export * as snsRepository from "./snsRepository";
